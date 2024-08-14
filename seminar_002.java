c313f01162a355380ebe4e6a3add2492d0ffa582
Dmitriy Demchuk <demchuk.btg@gmail.com>
2024-08-14 11:20:38 +0300

    Seminar 2, Task 1 (Update), Java dev

--- seminar_002.java
+++ seminar_002.java
@@ -7,22 +7,28 @@ public class seminar_002 {
 
         System.out.print("Введите четное число N, которое больше нуля: ");
         int numberN = ScannerUtil.enterInteger();
-        if (numberN <= 0) {
+        System.out.print("Введите первый символ: ");
+        char c1 = ScannerUtil.enterChar();
+        System.out.print("Введите второй символ: ");
+        char c2 = ScannerUtil.enterChar();
+        checkCorrectEnteredNumber(numberN, c1, c2);
+        fillStringWithChar(numberN, c1, c2);
+    }
+    private static void checkCorrectEnteredNumber(int number, char sumbolOne, char sumbolTwo){
+        if (number <= 0) {
             System.out.println("Введенное число меньше либо равно 0, что не соответствует условию!");
         }
-        else if (numberN % 2 != 0) {
+        else if (number % 2 != 0) {
             System.out.println("Введенное число не является четным, что не соответствует условию!");
-        } else {
-            System.out.print("Введите первый символ: ");
-            char c1 = ScannerUtil.enterChar();
-            System.out.print("Введите второй символ: ");
-            char c2 = ScannerUtil.enterChar();
-            for (int i = 0; i < numberN; i++) {
-                if (i % 2 == 0) {
-                    System.out.print(c1);
-                } else {
-                    System.out.print(c2);
-                }
+        }
+    }
+
+    private static void fillStringWithChar(int number, char sumbolOne, char sumbolTwo){
+        for (int i = 0; i < number; i++) {
+            if (i % 2 == 0) {
+                System.out.print(sumbolOne);
+            } else {
+                System.out.print(sumbolTwo);
             }
         }
     }
