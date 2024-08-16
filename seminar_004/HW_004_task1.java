17d8e2447db9c41b3531c73a96215c9853ee71ae
Dmitriy Demchuk <demchuk.btg@gmail.com>
2024-08-16 16:15:53 +0300

    Seminar 4, HW 4, Task 1, Java Dev

---
+++ seminar_004/HW_004_task1.java
@@ -0,0 +1,23 @@
+package seminar_004;
+
+import java.util.LinkedList;
+
+public class HW_004_task1 {
+    
+    public static void revertLL(LinkedList<Object> ll) {
+        LinkedList<Object> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add("One");
+        linkedList.add(2);
+        linkedList.add("Two");
+        linkedList.add('<');
+        
+        for (int i = 0; i < linkedList.size() / 2; i++) {
+            Object temp = linkedList.get(i);
+            linkedList.set(i, linkedList.size() - i);
+            linkedList.set(linkedList.size() - i, temp);
+        }
+        System.out.println(linkedList);
+}
+
+}
