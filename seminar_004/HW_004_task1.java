b3928097d169045c293065d7f2a33f09e36e1d64
Dmitriy Demchuk <demchuk.btg@gmail.com>
2024-08-16 16:51:47 +0300

    Seminar 4, HW 4, Task 1, Java Dev

--- seminar_004/HW_004_task1.java
+++ seminar_004/HW_004_task1.java
@@ -4,19 +4,23 @@ import java.util.LinkedList;
 
 public class HW_004_task1 {
     
-    public static void revertLL(LinkedList<Object> ll) {
+    public static void main(String[] args) {
         LinkedList<Object> linkedList = new LinkedList<>();
         linkedList.add(1);
         linkedList.add("One");
         linkedList.add(2);
         linkedList.add("Two");
         linkedList.add('<');
-        
-        for (int i = 0; i < linkedList.size() / 2; i++) {
-            Object temp = linkedList.get(i);
-            linkedList.set(i, linkedList.size() - i);
-            linkedList.set(linkedList.size() - i, temp);
+        linkedList.add('<');
+        revertLL(linkedList);
+    }
+
+    public static void revertLL(LinkedList<Object> ll) {
+        for (int i = 0; i < ll.size() / 2; i++) {
+            Object temp = ll.get(i);
+            ll.set(i, ll.get(ll.size() - i - 1));
+            ll.set(ll.size() - i - 1, temp);
         }
-        System.out.println(linkedList);
+        System.out.println(ll);
     }
 }
